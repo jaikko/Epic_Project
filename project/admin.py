af363f78fee1b40f27f0a384b52556e596d81f12
jaikko <65661383+jaikko@users.noreply.github.com>
2021-09-06 17:14:54 +0200

    Update admin.py

--- project/admin.py
+++ project/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import Staff
+from .models import Client, Contract, Event, Staff, Status
 
 # Register your models here.
 
@@ -21,4 +21,34 @@ class CustomUserAdmin(UserAdmin):
     search_fields = ('email',)
     ordering = ('email',)
 
+
+class ClientAdmin(admin.ModelAdmin):
+    model = Client
+    list_display = ('email', 'first_name', 'last_name',)
+    search_fields = ('email', 'first_name', 'last_name',)
+    ordering = ('email', 'first_name', 'last_name',)
+
+
+class StatusAdmin(admin.ModelAdmin):
+    model = Client
+    list_display = ('status',)
+    search_fields = ('status',)
+    ordering = ('status',)
+
+
+class EventAdmin(admin.ModelAdmin):
+    model = Event
+    list_display = ('client', 'attendees', 'event_date',)
+    ordering = ('client', 'attendees', 'event_date',)
+
+
+class ContractAdmin(admin.ModelAdmin):
+    model = Contract
+    list_display = ('client', 'status', 'amount', 'payment_due',)
+    ordering = ('client', 'status', 'amount', 'payment_due',)
+
 admin.site.register(Staff, CustomUserAdmin)
+admin.site.register(Client, ClientAdmin)
+admin.site.register(Status, StatusAdmin)
+admin.site.register(Event, EventAdmin)
+admin.site.register(Contract, ContractAdmin)
