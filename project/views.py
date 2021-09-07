84102b0f43a028df8a2491d01375f8f9098721f1
jaikko <65661383+jaikko@users.noreply.github.com>
2021-09-07 15:21:39 +0200

    Update views.py

--- project/views.py
+++ project/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django_filters.filters import CharFilter, DateFromToRangeFilter, RangeFilter
+from django_filters.filters import CharFilter, DateFilter, DateFromToRangeFilter, RangeFilter
+from rest_framework.fields import DateField
 from .serializers import *
 from rest_framework import viewsets
 from rest_framework import generics, permissions
@@ -13,18 +14,22 @@ from django_filters import rest_framework as filters
 
 
 class ContractFilter(filters.FilterSet):
+    amount_exact = CharFilter(field_name='amount', lookup_expr='exact')
     amount = RangeFilter()
+    payment_due_exact = DateFilter(field_name="payment_due", lookup_expr="date")
     payment_due = DateFromToRangeFilter()
     client = CharFilter(field_name='client__email', lookup_expr='exact')
     sale_contact = CharFilter(field_name='sale_contact__email', lookup_expr='exact')
 
     class Meta:
         model = Contract
-        fields = ['status', 'amount', 'payment_due', 'client', 'sale_contact']
+        fields = ['status', 'amount', 'payment_due', 'client', 'sale_contact', 'amount_exact', 'payment_due_exact']
 
 
 class EventFilter(filters.FilterSet):
+    attendees_exact = CharFilter(field_name='attendees', lookup_expr='exact')
     attendees = RangeFilter()
+    event_date_exact = DateFilter(field_name="event_date", lookup_expr="date")
     event_date = DateFromToRangeFilter()
     client = CharFilter(field_name='client__email', lookup_expr='exact')
     support_contact = CharFilter(field_name='support_contact__email', lookup_expr='exact')
